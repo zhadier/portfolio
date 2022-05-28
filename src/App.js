3a3999ffc5eec6ace754a0cdfd92e766ffa741b8
Zeeshan Haider <zhadier39@gmail.com>
2022-05-28 21:20:17 +0500

    Add React project and setup canvas design

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Canvas from './components/canvas/Canvas';
+
+function App() {
+  return <Canvas />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
